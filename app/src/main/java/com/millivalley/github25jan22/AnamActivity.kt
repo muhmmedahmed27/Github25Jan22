db1a673e9fa7bb3a3095301215536216b9360918
user <anamnaveed689@gmail.com>
2022-01-25 15:43:30 +0500

    Anam Commit 123

---
+++ app/src/main/java/com/millivalley/github25jan22/AnamActivity.kt
@@ -0,0 +1,11 @@
+package com.millivalley.github25jan22
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AnamActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_anam)
+    }
+}
