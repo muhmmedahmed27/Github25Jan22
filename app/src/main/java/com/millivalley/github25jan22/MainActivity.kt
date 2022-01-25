cb93083315b1bcee2082e41409411f28430421eb
Muhammed_Ahmed <muhammedahmedtariq279@gmail.com>
2022-01-25 15:20:40 +0500

    default first commit

---
+++ app/src/main/java/com/millivalley/github25jan22/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.millivalley.github25jan22
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
